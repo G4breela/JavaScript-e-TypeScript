cef9f1acc7101c4181c693322d83ef9baa2eb7b0
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-22 14:16:32 -0300

    aprendendo sobre constantes
    e também vimos que podemos utilizar o valor de uma constante dentro de um outra constante

---
+++ aula 7/index.js
@@ -0,0 +1,8 @@
+// * mesmas regras que para as variaveis com let
+// ! DIFRENÇA: constantes não podem ser redeclaradas e não podem ter o valor alterado e precisam ser inicializadas com um valor
+
+const nome = "João"; 
+const sobrenome = "Silva"
+const nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
