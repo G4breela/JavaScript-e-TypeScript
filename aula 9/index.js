38f322901e9182dcdecd6779720483b74f5cebb0
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-23 19:47:31 -0300

    Create index.js
    diferença entre let e var

---
+++ aula 9/index.js
@@ -0,0 +1,10 @@
+//var nome = "luiz";
+// var nome = "otavio"; // ! redeclarando variavel, não é uma boa prática. UTILIZE O LET
+console.log(nome);
+
+// nome = 'luiz'; // ! Não faça isso, está criando uma viavel global que pode causar um problema no código 
+
+// * utilize Let
+
+let nome = 'luiz'; 
+// let nome = 'mariana'; // * o let não pode ser redeclarado 
