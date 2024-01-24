c559a05551fc2700ece0263beab96aedfc7294f4
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-01-24 19:47:22 -0300

    Update script.js
    prompt, alert e confirm

--- aula 11/js/script.js
+++ aula 11/js/script.js
@@ -1,1 +1,11 @@
-alert('Opa, bão?');
+let oi = alert('Opa, bão?');
+
+let resposta= confirm('tem certeza disso?');
+
+if(resposta == true){
+    alert('Você é um bom mentiroso')
+}else{
+    let nome = prompt('diga seu nome: ');
+    alert(`Oi ${nome}, estou ligando para o seu psiquiatra!`)
+}
+
